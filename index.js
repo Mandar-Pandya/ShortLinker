422e7d1174e9deeea1a2e5f67e3349c3d370312d
Mandar Pandya <mandarpandya54@gmail.com>
2024-06-04 18:46:00 +0530

    feat: added URL shortener UI and functionality in home view file.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
+const path = require('path')
+const staticRoute = require('./routes/staticRouter')
+
 const { connectToMongoDB } = require("./connect");
 const app = express();
 const PORT = 8000;
@@ -8,8 +11,14 @@ connectToMongoDB("mongodb://127.0.0.1:27017/shortLinker").then(() =>
   console.log("connected to Db")
 );
 
+app.set('view engine','ejs')
+app.set('views',path.resolve('./views'))
+
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
 app.use("/url", urlRoute);
+app.use('/',staticRoute)
 
 app.listen(PORT, () => {
   console.log(`listening on port no ${PORT}`);
